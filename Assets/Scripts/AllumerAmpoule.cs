5da4dc6b6a209aa8b4b80b9c3ee72f4666021c50
CHERCHOUR Camille <98390331+Mflnn@users.noreply.github.com>
2024-10-06 16:30:46 +0200

    YarnSpinner Dialogues

--- Assets/Scripts/AllumerAmpoule.cs
+++ Assets/Scripts/AllumerAmpoule.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class AllumerAmpoule : MonoBehaviour
 {
     public Material eteint;
     public Material allume;
     public GameObject child;
+    [SerializeField] private DialogueRunner _dialogueSystem;
+    private bool dejaParle = false;
 
     private void Start() 
     {
@@ -15,5 +18,17 @@ public class AllumerAmpoule : MonoBehaviour
     public void Allumer()
     {
         child.GetComponent<MeshRenderer>().material = allume;
+       if(dejaParle == false)
+       {
+        StartDialogueAmpoule();
+       } 
     }
+
+    public void StartDialogueAmpoule()
+    {
+    _dialogueSystem.StartDialogue("Ampoule_001");
+        DialogFollow.SetTarget(gameObject);
+        dejaParle = true;
+    }
+
 }
