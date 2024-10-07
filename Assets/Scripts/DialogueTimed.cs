0d813a49635ce1c4e39621184de49cb7d2a50d3e
Paul Viudes <paulviudes17@gmail.com>
2024-10-07 16:50:50 +0200

    DialogTimed

---
+++ Assets/Scripts/DialogueTimed.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn;
+using Yarn.Unity;
+
+public class DialogueTimed : MonoBehaviour
+{
+    public string dialogName;
+    public float time;
+
+    DialogueRunner dialogueRunner;
+
+    private void Start()
+    {
+        dialogueRunner = GetComponent<DialogueRunner>();
+        StartCoroutine(delayDialog());
+    }
+    IEnumerator delayDialog()
+    {
+        yield return new WaitForSeconds(time);
+        dialogueRunner.StartDialogue(dialogName);
+    }
+}
